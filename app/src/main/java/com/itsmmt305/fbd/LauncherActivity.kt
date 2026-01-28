a8606fbdb32fe2e5cd9d8ec13a306d9b3f80e771
itsmmt305 <itsmmt305@gmail.com>
2026-01-29 02:50:56 +0530

    landing page update

---
+++ app/src/main/java/com/itsmmt305/fbd/LauncherActivity.kt
@@ -0,0 +1,13 @@
+package com.itsmmt305.fbd
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LauncherActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_launcher)
+    }
+}
