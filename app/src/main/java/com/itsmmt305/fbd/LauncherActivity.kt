9738bef80f7f8cb20d30c7149a53d76db7640ba7
itsmmt305 <itsmmt305@gmail.com>
2026-01-29 13:27:32 +0530

    closes #7

--- app/src/main/java/com/itsmmt305/fbd/LauncherActivity.kt
+++ app/src/main/java/com/itsmmt305/fbd/LauncherActivity.kt
@@ -1,13 +1,44 @@
 package com.itsmmt305.fbd
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.net.toUri
 
 class LauncherActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_launcher)
+
+        val btn = findViewById<Button>(R.id.app_open)
+        btn.setOnClickListener {
+            openApp()
+        }
+    }
+
+    private fun openApp() {
+        val pm : PackageManager = packageManager
+
+        try {
+            // Try to open FBA
+            pm.getPackageInfo("com.facebook.katana", 0)
+
+            val intent = Intent(Intent.ACTION_VIEW).apply {
+                data = "fb://feed".toUri()
+                setPackage("com.facebook.katana")
+            }
+            startActivity(intent)
+        } catch (e: PackageManager.NameNotFoundException) {
+
+            // FBA not installed -> open in browser
+            val intent = Intent(
+                Intent.ACTION_VIEW,
+                "https://www.facebook.com".toUri()
+            )
+            startActivity(intent)
+        }
     }
 }
